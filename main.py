b5227633610bcc5189b81a07f63c2a539ae2ebe4
a-maystorov <alkin971@gmail.com>
2025-11-30 13:37:08 +0100

    limit framerate to 60 fps

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -17,6 +19,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
